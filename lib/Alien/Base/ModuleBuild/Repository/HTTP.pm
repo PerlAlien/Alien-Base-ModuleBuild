70391192a97e607080fdf3854e71d0f010282558
Graham Ollis <plicease@cpan.org>
2018-08-29 16:44:33 -0400

    extra daignostic for how to fix ssl error

--- lib/Alien/Base/ModuleBuild/Repository/HTTP.pm
+++ lib/Alien/Base/ModuleBuild/Repository/HTTP.pm
@@ -162,6 +162,10 @@ sub check_http_response {
   else {
     if ( !$res->{success} ) {
       my $reason = $res->{status} == 599 ? $res->{content} : "@{[ $res->{status} ]} @{[ $res->{reason} ]}";
+      if($res->{status} == 599 && $reason =~ /https support/)
+      {
+        $reason .= "See https://github.com/Perl5-Alien/Alien-Base-ModuleBuild/issues/6#issuecomment-417097485";
+      }
       return ( 1, $reason, $res->{headers}, $res->{url} );
     }
     return ( 0, $res->{content}, $res->{headers}, $res->{url} );
