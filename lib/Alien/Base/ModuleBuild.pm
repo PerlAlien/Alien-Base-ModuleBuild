6c97df114cbfebff9b9477d72da3fbd3083d8bee
Graham Ollis <plicease@cpan.org>
2020-02-04 13:46:27 -0700

    use strict and warnings in generates Install::Files

--- lib/Alien/Base/ModuleBuild.pm
+++ lib/Alien/Base/ModuleBuild.pm
@@ -359,6 +359,8 @@ sub ACTION_code {
     open my $fh, '>', $file;
     print $fh <<EOF;
 package $module\::Install::Files;
+use strict;
+use warnings;
 require $module;
 sub Inline { shift; $module->Inline(\@_) }
 1;
